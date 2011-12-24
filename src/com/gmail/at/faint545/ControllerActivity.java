7d785500c3f763109b59cbab570609f1ab2af423
Alex Fu <alexfu@bounty-board.com>
2011-12-24 11:30:54 -0500

    Initial commit

---
+++ src/com/gmail/at/faint545/ControllerActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.at.faint545;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ControllerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
